37f2c04627e51d0c3d2db8230f8f0bd4fe973c6c
iAmTheDolphin <parkeradamjones@gmail.com>
2018-02-03 16:32:04 -0600

    Initialized repo

---
+++ heap.h
@@ -0,0 +1,15 @@
+//
+// Created by Parker Jones on 2/1/18.
+//
+
+#ifndef ASSIGN1_HEAP_H
+#define ASSIGN1_HEAP_H
+
+#include "dll.h"
+
+typedef struct treeNode TREENODE;
+typedef struct heap HEAP;
+
+
+
+#endif //ASSIGN1_HEAP_H
